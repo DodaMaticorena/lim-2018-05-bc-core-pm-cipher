d633d4101ac9809a6de41a4b80e4993bd6b290ad
DodaMaticorena <doda.maticorena@gmail.com>
2018-05-25 16:32:17 -0500

    intento de js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 //Funciones y declaramos las variables
-=> {
- const texto = document.getElementById('textoEntrada');   
-}
+const texto = document.getElementById('textoEntrada');  
 
 const numeroDesplazamiento = parseInt(document.getElementById('input'));
-const cifrar = document.getElementById('botonCifrado');
+
+function encriptado (texto, numeroDesplazamiento) {
+  const formulaCifrar = 
+  const cifrar = document.getElementById('botonCifrado');
+    cifrar.addEventListener('click', formulaCifrar)  
+
+  for (let a = 0; a < texto.length; a++){
+    texto = 
+  }
+}
 const descifrar = document.getElementById('botonDescifrado');
+
 const texto2 = document.getElementById('textoSalida');
