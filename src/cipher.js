8e83da0f936774c10d5901d72da4ae775de234b8
DodaMaticorena <doda.maticorena@gmail.com>
2018-05-29 18:22:48 -0500

    sigo el avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,11 @@
-window.cipher = {
+// window.cipher = {
   // ... 
-};
+// };
+
+function encode(texto,numeroDesplazamiento) {
+  let textoCifrar = []
+  for (let a = 0; a < texto.length; a++){
+    const letras = "ABC"
+  }
+}
+if (inputWord.match(/[A-Za-Z/])
--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
+import { Z_ASCII } from "zlib";
+
 //Funciones y declaramos las variables
 let texto = document.getElementById('textoEntrada');  
-let numeroDesplazamiento = document.getElementById('input');
+let numeroDesplazamiento = document.getElementById('numDesplaz');
 const cifrar = document.getElementById('botonCifrado');
 let texto2 = document.getElementById('textoSalida');
 
+// if (texto.match([A-Za-Z])
 
-function encriptado (texto, numeroDesplazamiento) { 
-  
-    texto.addEventListener('input', ()=>{
+texto.addEventListener('input', ()=> {
       console.log(texto.value)
     }  )
+texto.addEventListener("input", ()=> {
+      console.log(numeroDesplazamiento.value)
+    } )
 
+
+cifrar.addEventListener("click", ()=> {
+  texto2.innerHTML = encode(texto, numeroDesplazamiento);
+} )
+
+function encode(texto,numeroDesplazamiento) {
+  let textoCifrar = [];
+  const asc1;
+  const asc2;
   for (let a = 0; a < texto.length; a++){
-    console.log(texto[a]) 
+    const letras = texto.match([A-Za-z]);
+
   }
 }
+
+
 const descifrar = document.getElementById('botonDescifrado');
 
