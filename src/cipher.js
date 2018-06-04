054d021eab7571d95b37804c8dd05135fdb54ad7
DodaMaticorena <doda.maticorena@gmail.com>
2018-06-04 00:56:27 -0500

    ya casi

--- src/cipher.js
+++ src/cipher.js
@@ -1,38 +1,49 @@
+//Variable para establecer la funcionalidad del cifrado
 const cod = (offset, string) => {
   let exit = '';
   let firstAsci = 0;
   let formula = 0;
   let offset1 = parseInt(offset);
-  //let lastAsci = '';
-//debugger
+  let lastAsci = "";
+
   for (let a = 0; a < string.length; a++){
     firstAsci = string.charCodeAt(a);
     if(64 < firstAsci && firstAsci < 91){
       formula = (firstAsci - 65 + offset1) % 26 + 65;
-      exit = exit.concat(String.fromCharCode(formula));
+      lastAsci = String.fromCharCode(formula);
+      exit += lastAsci;
+      //o puede ser sin la variable lastAsci: exit = exit.concat(String.fromCharCode(formula));
     } else {
-      exit = '?';
+        formula[a] = firstAsci; 
     }
   }
   return exit
 }
 
+//Variable para establecer la funcionalidad del descifrado
 const decod = (offset, string) => {
    let exit = '';
    let firstAsci = 0;
    let formula = 0;
    let offset1 = parseInt(offset);
-   //let lastAsci = '';
-
+   let lastAsci = "";
+  
    for (let a = 0; a < string.length; a++){
-    firstAsci = string.charCodeAt(a);
-    formula = (firstAsci - 65 - offset1) % 26 + 65;
-    exit = exit.concat(String.fromCharCode(formula));
+     firstAsci = string.charCodeAt(a);
+     if(64 < firstAsci && firstAsci < 91){
+       formula = (firstAsci - 65 - (offset1 % 26) + 26) % 26 + 65;
+       lastAsci = String.fromCharCode(formula);
+       exit += lastAsci;
+       //también puede ser sin la variable lastAsci: exit = exit.concat(String.fromCharCode(formula))
+     } else {
+        formula[a] = firstAsci; 
+     }
    }
    return exit
 
 }
 
+//window
 window.cipher = {
   encode: cod,
   decode: decod,
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ let offset = document.getElementById('offset');
 const encrypt = document.getElementById('butEncrypt');
 let text2 = document.getElementById('exitText');
 
-// if (texto.match([A-Za-Z])
-
+//Escuchamos al evento click para obtener el resultado del cifrado
 encrypt.addEventListener("click", ()=> {
   text2.value = cipher.encode(offset.value, string.value);
   text2.innerHTML = text2.value;
 } )
 
+//Variable de descifrado
 const decrypt = document.getElementById('butDecrypt');
 
+//Escuchamos al evento click para obtener el resultado del descifrado
 decrypt.addEventListener("click", ()=> {
   text2.value = cipher.decode(offset.value, string.value);
   text2.innerHTML = text2.value;
