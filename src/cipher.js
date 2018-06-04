a87e1a835d706bbf38397d462d0fa23d49ab8772
DodaMaticorena <doda.maticorena@gmail.com>
2018-06-04 02:43:21 -0500

    falta espacios entre palabras

--- src/cipher.js
+++ src/cipher.js
@@ -4,43 +4,38 @@ const cod = (offset, string) => {
   let firstAsci = 0;
   let formula = 0;
   let offset1 = parseInt(offset);
-  let lastAsci = "";
 
   for (let a = 0; a < string.length; a++){
     firstAsci = string.charCodeAt(a);
     if(64 < firstAsci && firstAsci < 91){
       formula = (firstAsci - 65 + offset1) % 26 + 65;
-      lastAsci = String.fromCharCode(formula);
-      exit += lastAsci;
-      //o puede ser sin la variable lastAsci: exit = exit.concat(String.fromCharCode(formula));
+      exit = exit.concat(String.fromCharCode(formula));
+      //o puede ser con otra variable lastAsci: lastAsci = String.fromCharCode(formula); y exit += lastAsci;
     } else {
-        formula[a] = firstAsci; 
-    }
+        
+    } 
   }
   return exit
 }
 
 //Variable para establecer la funcionalidad del descifrado
 const decod = (offset, string) => {
-   let exit = '';
-   let firstAsci = 0;
-   let formula = 0;
-   let offset1 = parseInt(offset);
-   let lastAsci = "";
+  let exit = '';
+  let firstAsci = 0;
+  let formula = 0;
+  let offset1 = parseInt(offset);
   
-   for (let a = 0; a < string.length; a++){
-     firstAsci = string.charCodeAt(a);
-     if(64 < firstAsci && firstAsci < 91){
-       formula = (firstAsci - 65 - (offset1 % 26) + 26) % 26 + 65;
-       lastAsci = String.fromCharCode(formula);
-       exit += lastAsci;
-       //también puede ser sin la variable lastAsci: exit = exit.concat(String.fromCharCode(formula))
-     } else {
-        formula[a] = firstAsci; 
-     }
-   }
+  for (let a = 0; a < string.length; a++){
+    firstAsci = string.charCodeAt(a);
+    if(64 < firstAsci && firstAsci < 91){
+      formula = (firstAsci - 65 - (offset1 % 26) + 26) % 26 + 65;
+      exit = exit.concat(String.fromCharCode(formula));
+    } else {
+        
+        //formula[a] = " ";
+    } 
+  }
    return exit
-
 }
 
 //window
