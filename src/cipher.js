77a757a03679b2a2210da2a0b9d3f88228e4eb41
DodaMaticorena <doda.maticorena@gmail.com>
2018-06-05 16:38:28 -0500

    ahora con minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -11,6 +11,9 @@ const cod = (offset, string) => {
       formula = (firstAsci - 65 + offset1) % 26 + 65;
       exit = exit.concat(String.fromCharCode(formula));
       //o puede ser con otra variable lastAsci: lastAsci = String.fromCharCode(formula); y exit += lastAsci;
+    } if (96 < firstAsci && firstAsci < 123){
+         formula = (firstAsci - 97 + offset1) % 26 + 97;
+         exit = exit.concat(String.fromCharCode(formula));
     } else if (firstAsci === 32) {
         exit = exit.concat(String.fromCharCode(32));
     } 
@@ -29,6 +32,9 @@ const decod = (offset, string) => {
     firstAsci = string.charCodeAt(a);
     if(64 < firstAsci && firstAsci < 91){
       formula = (firstAsci - 65 - (offset1 % 26) + 26) % 26 + 65;
+      exit = exit.concat(String.fromCharCode(formula));
+    } if (96 < firstAsci && firstAsci < 123){
+      formula = (firstAsci - 97 - (offset1 % 26) + 26) % 26 + 97;
       exit = exit.concat(String.fromCharCode(formula));
     } else if (firstAsci === 32) {
         exit = exit.concat(String.fromCharCode(32));   
