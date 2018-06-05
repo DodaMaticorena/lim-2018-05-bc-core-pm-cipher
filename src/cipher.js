5bf358aeb54ffe4b2d19a8e3a4403e475a38ea47
DodaMaticorena <doda.maticorena@gmail.com>
2018-06-05 17:17:56 -0500

    intentando con el spanish

--- src/cipher.js
+++ src/cipher.js
@@ -12,8 +12,11 @@ const cod = (offset, string) => {
       exit = exit.concat(String.fromCharCode(formula));
       //o puede ser con otra variable lastAsci: lastAsci = String.fromCharCode(formula); y exit += lastAsci;
     } if (96 < firstAsci && firstAsci < 123){
-         formula = (firstAsci - 97 + offset1) % 26 + 97;
-         exit = exit.concat(String.fromCharCode(formula));
+        formula = (firstAsci - 97 + offset1) % 26 + 97;
+        exit = exit.concat(String.fromCharCode(formula));
+    } if (129 < firstAsci && firstAsci < 166) {
+        formula = (firstAsci - 130 + offset1) % 26 + 130;
+        exit = exit.concat(String.fromCharCode(formula));
     } else if (firstAsci === 32) {
         exit = exit.concat(String.fromCharCode(32));
     } 
@@ -34,8 +37,11 @@ const decod = (offset, string) => {
       formula = (firstAsci - 65 - (offset1 % 26) + 26) % 26 + 65;
       exit = exit.concat(String.fromCharCode(formula));
     } if (96 < firstAsci && firstAsci < 123){
-      formula = (firstAsci - 97 - (offset1 % 26) + 26) % 26 + 97;
-      exit = exit.concat(String.fromCharCode(formula));
+        formula = (firstAsci - 97 - (offset1 % 26) + 26) % 26 + 97;
+        exit = exit.concat(String.fromCharCode(formula));
+    } if (129 < firstAsci && firstAsci < 166){
+        formula = (firstAsci - 130 - (offset1 % 26) + 26) % 26 + 130;
+        exit = exit.concat(String.fromCharCode(formula));
     } else if (firstAsci === 32) {
         exit = exit.concat(String.fromCharCode(32));   
     } 
