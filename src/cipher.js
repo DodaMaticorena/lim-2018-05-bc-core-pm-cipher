c47cc132544fc990b6fac5b48a46cf06e45f5e82
DodaMaticorena <doda.maticorena@gmail.com>
2018-06-05 18:01:31 -0500

    algunos cambios

--- src/cipher.js
+++ src/cipher.js
@@ -3,15 +3,15 @@ const cod = (offset, string) => {
   let exit = '';
   let firstAsci = 0;
   let formula = 0;
-  let offset1 = parseInt(offset);
+  const offset1 = parseInt(offset);
 
-  for (let a = 0; a < string.length; a++){
+  for (let a = 0; a < string.length; a++) {
     firstAsci = string.charCodeAt(a);
-    if(64 < firstAsci && firstAsci < 91){
+    if(64 < firstAsci && firstAsci < 91) {
       formula = (firstAsci - 65 + offset1) % 26 + 65;
       exit = exit.concat(String.fromCharCode(formula));
       //o puede ser con otra variable lastAsci: lastAsci = String.fromCharCode(formula); y exit += lastAsci;
-    } if (96 < firstAsci && firstAsci < 123){
+    } if (96 < firstAsci && firstAsci < 123) {
         formula = (firstAsci - 97 + offset1) % 26 + 97;
         exit = exit.concat(String.fromCharCode(formula));
     } if (129 < firstAsci && firstAsci < 166) {
@@ -29,17 +29,17 @@ const decod = (offset, string) => {
   let exit = '';
   let firstAsci = 0;
   let formula = 0;
-  let offset1 = parseInt(offset);
+  const offset1 = parseInt(offset);
   
-  for (let a = 0; a < string.length; a++){
+  for (let a = 0; a < string.length; a++) {
     firstAsci = string.charCodeAt(a);
-    if(64 < firstAsci && firstAsci < 91){
+    if(64 < firstAsci && firstAsci < 91) {
       formula = (firstAsci - 65 - (offset1 % 26) + 26) % 26 + 65;
       exit = exit.concat(String.fromCharCode(formula));
-    } if (96 < firstAsci && firstAsci < 123){
+    } if (96 < firstAsci && firstAsci < 123) {
         formula = (firstAsci - 97 - (offset1 % 26) + 26) % 26 + 97;
         exit = exit.concat(String.fromCharCode(formula));
-    } if (129 < firstAsci && firstAsci < 166){
+    } if (129 < firstAsci && firstAsci < 166) {
         formula = (firstAsci - 130 - (offset1 % 26) + 26) % 26 + 130;
         exit = exit.concat(String.fromCharCode(formula));
     } else if (firstAsci === 32) {
